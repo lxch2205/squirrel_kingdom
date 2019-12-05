3a9e307938db78d5c0fa1458f260d0c51800d0f4
lxch2205 <lc3464@columbia.edu>
2019-12-05 20:39:43 +0000

    models.py edited

--- project/sightings/models.py
+++ project/sightings/models.py
@@ -36,6 +36,134 @@ class Squirrel(models.Models):
         default=AM, # ?
     )
 
+    date = models.DateField(
+        help_text=_('Date'),
+    )
+
+    ADULT = 'Adult'
+    JUVENILE = 'Juvenile'
+
+    AGE_CHOICES = (
+        (ADULT,'Adult'),
+        (JUVENILE,'Juvenile'),
+    )
+
+    age = models.CharField(
+        help_text=_('Age'),
+        max_length=16,
+        choices=AGE_CHOICES,
+        blank=True, #?
+        default=ADULT, #?
+    )
+
+    GRAY = 'Gray'
+    CINNAMON = 'Cinnamon'
+    BLACK = 'Black'
+
+    COLOR_CHOICES = (
+        (GRAY,'Gray'),
+        (CINNAMON,'Cinnamon'),
+        (BLACK,'Black'),
+    )
+
+    color = models.CharField(
+        help_text=_('Primary Fur Color'),
+        max_length=20,
+        choices=COLOR_CHOICES,
+        blank=True, #? 
+    )
+
+    GROUND_PLANE='Ground Plane'
+    ABOVE_GROUND='Above Ground'
+
+    LOCATION_CHOICES = (
+        (GROUND_PLANE,'Ground Plane'),
+        (ABOVE_GROUND,'Above Ground'),
+    )
+
+    location = models.CharField(
+        help_text=_('Location'),
+        max_length=20,
+        choices=LOCATION_CHOICES,
+        blank=True,
+        # default
+
+    specific_location = models.CharField(
+        help_text=_('Specific Location'),
+        max_length=255,
+        blank=True,
+    )
+
+    running = models.BooleanField(
+        help_text=_('Running'),
+        default=False,
+    )
+
+    chasing = models.BooleanField(
+        help_text=_('Chasing'),
+        default=False,
+    )
+    
+    climbing = models.BooleanField(
+        help_text=_('Climbing'),
+        default=False,
+    )
+
+    eating = models.BooleanField(
+        help_text=_('Eating'),
+        default=False,
+    )
+
+    foraging = models.BooleanField(
+        help_text=_('Foraging'),
+        default=False,
+    )
+
+    other_activities = models.CharField(
+        help_text=_('Other Activities'),
+        max_length=255,
+        blank=True,
+    )
+
+    kuks = models.BooleanField(
+        help_text=_('Kuks'),
+        default=False,
+    )
+
+    quaas = models.BooleanField(
+        help_text=_('Quaas'),
+        default=False,
+    )
+
+    moans = models.BooleanField(
+        help_text=_('Moans'),
+        default=False,
+    )
+
+    tail_flags = models.BooleanField(
+        help_text=_('Tail flags'),
+        default=False,
+    )
+
+    tail_twitches = models.BooleanField(
+        help_text=_('Tail twitches'),
+        default=False,
+    )
+
+    approaches = models.BooleanField(
+        help_text=_('Approaches'),
+        default=False,
+    )
+
+    indifferent = models.BooleanField(
+        help_text=_('Indifferent'),
+        default=False,
+    )
+
+    runs_from = models.BooleanField(
+        help_text=_('Runs from'),
+        default=False,
+    )
 
 
 
