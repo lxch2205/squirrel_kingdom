0bd1e539d28a47cb13b001d69c3a9b017c752676
JieMei <jm4994@columbia.edu>
2019-12-06 01:54:56 +0000

    views edited

--- project/sightings/views.py
+++ project/sightings/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from models import Squirrel
+from django.http import HttpResponse
 
+def catalog(request):
+    squirrels = Squirrel.objects.all()
+    context = {
+        'squirrels' : squirrels.order_by('id'),
+    }
+    return render
 # Create your views here.
