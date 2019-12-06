d13294105f89cbad8663ba6190a2998382259c49
lxch2205 <lc3464@columbia.edu>
2019-12-06 14:37:18 +0000

    edited views and urls

--- project/sightings/views.py
+++ project/sightings/views.py
@@ -1,21 +1,30 @@
-from django.shortcuts import render
-from models import Squirrel
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.views.generic import UpdateView
+from .models import Squirrel
+from .forms import SquirrelForm
 
-def catalog(request):
+def all_squirrels(request):
     squirrels = Squirrel.objects.all()
+    field_names = Squirrel._meta.get_fields()
     context = {
         'squirrels' : squirrels.order_by('id'),
+        'field_names': field_names
     }
-    return render(request,'sightings/catalog.html',context)
+    return render(request, 'sightings/all_squirrels.html', context)
 
 def add_squirrel(request):
     if request.method == 'POST':
         form = SquirrelForm(request.POST)
         #check data with form
         if form.is_valid():
+            x = form['id'].value()
             form.save()
-            return redirect(f'/sightings/add')
+            return redirect(f'/sightings/{x}')
+    else:
+        form = SquirrelForm()
+        return render(request, 'sightings/add.html',{'form': form})
 
 
 # Create your views here.
