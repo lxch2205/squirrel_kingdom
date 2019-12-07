7378db77e8c9ad9ea2f124c0ac60ae474706895e
JieMei <jm4994@columbia.edu>
2019-12-07 21:59:01 +0000

    views.py edited

--- project/sightings/views.py
+++ project/sightings/views.py
@@ -31,8 +31,42 @@ def add_squirrel(request):
 
 
 def stats(request):
-    dataset = Squirrel.objects \
-        .values('shift')\
-        .annotate(
+    total_squirrels = Squirrel.objects.count()
+    gray = Squirrel.objects.filter(color='Gray').count()
+    cinnamon = Squirrel.objects.filter(color='Cinnamon').count()
+    black = Squirrel.objects.filter(color='Black').count()
+    adult = Squirrel.objects.filter(age='Adult').count()
+    juvenile = Squirrel.objects.filter(age='Juvenile').count()
+    above_ground = Squirrel.objects.filter(location='Above Ground').count()
+    ground_plane = Squirrel.objects.filter(location='Ground Plane').count()
+    approaches = Squirrel.objects.filter(approaches='Approaches').count()
+    indifferent = Squirrel.objects.filter(indifferent='Indifferent').count()
+    runs_from = Squirrel.objects.filter(runs_from='Runs from').count()
+    running_true = Squirrel.objects.filter(running=True).count()
+    chasing_true = Squirrel.objects.filter(chasing=True).count()  
+    climbing_true = Squirrel.objects.filter(climbing=True).count()
+    eating_true = Squirrel.objects.filter(eating=True).count()
+    foraging_true = Squirrel.objects.filter(foraging=True).count()
+    #other_activities_true = Squirrel.objects.filter(other_activities=True).count()
+
+    context = {
+            'total_squirrels' : total_sightings,
+            'gray' :gray,
+            'cinnamon' : cinnamon
+            'black' : black
+            'adult' : adult
+            'juvenile' : juvenile
+            'above_ground' : above_ground
+            'ground_plane' : ground_plane
+            'approaches' : approaches
+            'indifferent' : indifferent
+            'runs_from' : run_from
+            'running_true' : running
+            'chasing_true' : chasing
+            'climbing_true' : climbing
+            'eating_true' : eating
+            'foraging_true' : foraging
+            }
+    return render(request, 'sightings.status.html', context)
 
 
