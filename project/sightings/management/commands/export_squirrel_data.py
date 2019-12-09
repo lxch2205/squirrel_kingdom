96d29d75da284867facca042bf8dabe9d32f3487
JieMei <jm4994@columbia.edu>
2019-12-09 18:19:19 +0000

    last change

--- project/sightings/management/commands/export_squirrel_data.py
+++ project/sightings/management/commands/export_squirrel_data.py
@@ -1,41 +1,25 @@
-import csv
-
 from django.core.management.base import BaseCommand
 from sightings.models import Squirrel
+from django.utils import timezone
 import pandas as pd
+import numpy as np
+import datetime
 
 class Command(BaseCommand):
-    def add_srguments(self, parser):
-        parser.add_argument('database')
+    def add_arguments(self, parser):
+        parser.add_argument('csv_file')
 
     def handle(self, *args, **options):
-        csv_file = list()
-        for item in Squirrel.objects.all():
-            csv_file.append(
-                {'Latitude':item.latitude,
-                'Longitude':item.longitude,
-                'Unique Squirrel id':item.id,
-                'Shift':item.shift,
-                'Date':item.date,
-                'Age':item.age,
-                'Primary Fur Color':item.color,
-                'Location':item.location,
-                'Specific Location':item.specific_location,
-                'Running':item.running,
-                'Chasing':item.chasing,
-                'Climbing':item.climbing,
-                'Eating':item.eating,
-                'Foraging':item.foraging,
-                'Other Activities':item.other_activities,
-                'Kuks':item.kuks,
-                'Quaas':item.quaas,
-                'Moans':item.moans,
-                'Tail Flag':item.tail_flag,
-                'Tail Twitches':item.tail_twitches,
-                'Approaches':item.approaches,
-                'Indifferent':item.indifferent,
-                'Runs From':item.runs_from}
-             )
-        file_export = pd.DataFrame(csv_export)
-        file_export.to_csv('2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv')
+        cols = ['X', 'Y', 'Shift', 'Date', 'Age', 'Primary Fur Color', 'Location', 'Specific Location', 'Running', 'Chasing', 'Climbing', 'Eating', 'Foraging', 'Other Activities', 'Kuks', 'Quaas', 'Moans', 'Tail flags', 'Tail twitches', 'Approaches', 'Indifferent', 'Runs from']
+        item_ = list()
+        total_items = list()
+        for item in Squirrel.objects.values():
+            item_ = list(item.values())[1:]
+            item_[0], item_[1] = item_[1], item_[0]
+            item_[3] = item_[3].strftime('%m%d%Y')
+            total_items.append(item_)
+
+        df = pd.DataFrame(total_items, columns=cols)
+        df.to_csv(options['csv_file'], index=False)
+
 
